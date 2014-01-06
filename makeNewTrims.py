e3fdc6c732c6fd34d44eec3646fb2ce2bd218877
Andy Becker <acbecker@gmail.com>
2014-01-06 13:22:36 -0600

    Adjust the brightesses of stars based upon their colors

--- makeNewTrims.py
+++ makeNewTrims.py
@@ -11,12 +11,17 @@ for line in open(trimIn).readlines():
         print line,
         continue
     
+    mag = line.split()[4]
+
     if (oid % 10) == 0:
         star = A0V
+        newmag = "%.5f" % (float(mag) -0.783)
     elif (oid % 10) == 5:
         star = M2V
+        newmag = "%.5f" % (float(mag) + 1.955)
     else:
         star = G0V
+        newmag = mag
 
-    print re.sub("starSED/kurucz/km50_5000.fits_g20_5140.gz", star, line), 
+    print re.sub("starSED/kurucz/km50_5000.fits_g20_5140.gz", star, re.sub(mag, newmag, line)), 
     oid += 1
