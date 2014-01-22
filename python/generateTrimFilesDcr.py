50cd44183a04412c172e66fde3df0788ab3c7d80
Andy Becker <acbecker@gmail.com>
2014-01-22 10:11:42 -0800

    Include a color correction to the magnitudes of the v7 sims

--- python/generateTrimFilesDcr.py
+++ python/generateTrimFilesDcr.py
@@ -3,6 +3,35 @@ import lsst.sims.catalogs.measures.example_utils as utils
 import numpy as np
 import sys, re
 
+
+# Come from colors.py
+colors_vs_r = {}
+for sed in ("kp01_9750.fits_g45_9830.gz",
+            "km20_6000.fits_g30_6020.gz",
+            "m2.0Full.dat.gz"):
+    colors_vs_r[sed] = {}
+colors_vs_r["kp01_9750.fits_g45_9830.gz"]["u"] =  0.60680054126
+colors_vs_r["kp01_9750.fits_g45_9830.gz"]["g"] = -0.23920305940
+colors_vs_r["kp01_9750.fits_g45_9830.gz"]["r"] =  0.0
+colors_vs_r["kp01_9750.fits_g45_9830.gz"]["i"] =  0.23364024549
+colors_vs_r["kp01_9750.fits_g45_9830.gz"]["z"] =  0.41330200452
+colors_vs_r["kp01_9750.fits_g45_9830.gz"]["y"] =  0.45125140946
+
+colors_vs_r["km20_6000.fits_g30_6020.gz"]["u"] =  1.21602658889
+colors_vs_r["km20_6000.fits_g30_6020.gz"]["g"] =  0.30637498242
+colors_vs_r["km20_6000.fits_g30_6020.gz"]["r"] =  0.0
+colors_vs_r["km20_6000.fits_g30_6020.gz"]["i"] = -0.10157478397
+colors_vs_r["km20_6000.fits_g30_6020.gz"]["z"] = -0.11823111346
+colors_vs_r["km20_6000.fits_g30_6020.gz"]["y"] = -0.11561614594
+
+colors_vs_r["m2.0Full.dat.gz"]["u"] =  3.6508634144
+colors_vs_r["m2.0Full.dat.gz"]["g"] =  1.3542089873
+colors_vs_r["m2.0Full.dat.gz"]["r"] =  0.0
+colors_vs_r["m2.0Full.dat.gz"]["i"] = -1.0173818086
+colors_vs_r["m2.0Full.dat.gz"]["z"] = -1.4889633446 
+colors_vs_r["m2.0Full.dat.gz"]["y"] = -1.6751067151
+
+
 fMapper = {"g": 1, "r": 2, "i": 3}
 
 raRad     = 79.6892650466 * np.pi / 180
@@ -52,6 +81,10 @@ for imjd, mjd in enumerate(mjds):
                 value = value[0] * 180. / np.pi  # turn from rad into degrees
             buff.write("%s %s\n" % (key, value))
         for line in newlines:
+            fields = line.split()
+            sedKey = fields[5].split("/")[-1]
+            rmag   = fields[4]
+            line   = re.sub(rmag, str(float(rmag)-colors_vs_r[sedKey][filt]), line)
             buff.write(line)
         buff.close()
         #print obshistid, params
