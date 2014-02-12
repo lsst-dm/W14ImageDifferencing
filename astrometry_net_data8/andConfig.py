2b3f722dc0af9d63f2eacf47f725576c345d13bc
Andy Becker <acbecker@gmail.com>
2014-02-12 12:58:10 -0600

    Fixing andConfig to point to correct index files

--- astrometry_net_data8/andConfig.py
+++ astrometry_net_data8/andConfig.py
@@ -3,9 +3,9 @@ filters = ('u', 'g', 'r', 'i', 'z', 'y')
 root.magColumnMap = dict([(f,f) for f in filters])
 root.magErrorColumnMap = dict([(f, f + '_err') for f in filters])
 root.indexFiles = [
-    'index-140108000.fits',  
-    'index-140108001.fits',  
-    'index-140108002.fits',  
-    'index-140108003.fits',
-    'index-140108004.fits',
+    'index-140212000.fits',  
+    'index-140212001.fits',  
+    'index-140212002.fits',  
+    'index-140212003.fits',
+    'index-140212004.fits',
     ]
